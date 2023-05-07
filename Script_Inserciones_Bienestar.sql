30cc02016c28b2886741349e2dbe5415c03415f7
Javier Tarazona <96489034+JavierTarazona06@users.noreply.github.com>
2023-05-06 20:38:18 -0500

    Datos de base para trabajar

--- Script_Inserciones_Bienestar.sql
+++ Script_Inserciones_Bienestar.sql
@@ -1,3 +1,5 @@
+use bienestar;
+
 #Javier
 #------------------------------------------------------------------
 
@@ -112,25 +114,50 @@ insert into estudiante (estID,carreID,estEdad,estFacultad,estPBM,estTipoAdmision
 insert into estudiante (estID,carreID,estEdad,estFacultad,estPBM,estTipoAdmision,estEsEgresado) values (29,'20','28','Ciencias Veterinarias y de Zootecnia','90','PEAMA','0');
 insert into estudiante (estID,carreID,estEdad,estFacultad,estPBM,estTipoAdmision,estEsEgresado) values (30,'29','30','Ciencias','96','PEAMA','0');
 
-insert into area values ('Gestión y Fomento Socieconómico',32132,'economicaagfse_bog@unal.edu.co',1,'Unidad Camilo Torres','El Área de Gestión y Fomento Socioeconómico promueve el mejoramiento de las condiciones socioeconómicas y el desarrollo personal y profesional de la comunidad estudiantil.','http://bienestar.bogota.unal.edu.co/gestion_actas.php#docs');
+insert into bienestar.area values
+(1, "Salud", 5713165000, "areasalud_bog@unal.edu.co", 21072, "CAN Bloque 3, Edificio 933",
+"El Área de Salud promueve el mejoramiento permanente de las condiciones físicas, psíquicas, mentales, sociales y ambientales en las que se desarrolla la vida universitaria, mediante programas formativos, preventivos y correctivos que incidan en la calidad de vida y hábitos de vida saludable.
+Protegemos y promocionamos la salud integral de la comunidad universitaria a partir de la caracterización del riesgo individual y grupal, la prevención de la enfermedad, la gestión en salud y la atención de emergencias."),
+(2,"Cultura", 5713165194, "culturabien_bog@unal.edu.co", 17248, "Edificio No. 103
+Polideportivo Primer Piso / antiguas oficinas División de archivo", 
+"El Área de Cultura promueve en la comunidad universitaria el desarrollo de aptitudes estéticas y artísticas en su formación, expresión y divulgación, atendiendo la diversidad cultural de la misma.
+Estimulamos la formación y la proyección artística de la comunidad universitaria, en diversas expresiones modernas, contemporáneas, urbanas y representativas del folclor nacional, cultivando el respeto por el patrimonio
+ cultural material e inmaterial, el reconocimiento de la diversidad y la creación e investigación de nuevas prácticas del saber."),
+ (3,"Actividad física y Deporte",3165000, "divrecd_bog@unal.edu.co", 17207, "Edificio No. 103 Polideportivo",
+ "El Área de Actividad Física y Deporte estimula la práctica de actividades de tipo recreativo, formativo y competitivo, en el marco del sano esparcimiento, la promoción de la salud, la utilización racional y formativa del tiempo libre y la implementación del deporte universitario.
+Fomentamos la actividad física, el acondicionamiento físico dirigido y el rendimiento deportivo mediante la articulación de las actividades deportivas con la vida académica para apoyar la formación integral y afirmar el sentido de pertenencia hacia la Universidad."),
+(4, "Acompañamiento integral", 3165000, "aaintegral@unal.edu.co", 17171, "Ciudad Universitaria - Edificio 103 - Polideportivo Segundo piso",
+"El Área de Acompañamiento Integral desarrolla acciones orientadas a los miembros de la comunidad para apoyar su paso por la Universidad, facilitar el conocimiento de sí mismo y de los demás; desarrollar el sentido de pertenencia,
+ el compromiso individual, la construcción de un tejido social incluyente y el fortalecimiento de las relaciones humanas.
+Un equipo humano interdisciplinario que orienta el fortalecimiento del potencial de los miembros de la comunidad universitaria en los cambios y retos que le impone su tránsito por la institución."),
+(5,"Gestión y fomento socieconómico", 3165000, "economicaagfse_bog@unal.edu.co", 10652, "Unidad Camilo Torres Bloque B 7, Oficina: 201", 
+"El Área de Gestión y Fomento Socioeconómico promueve el mejoramiento de las condiciones socioeconómicas y el desarrollo personal y profesional de la comunidad estudiantil."),
+(6, "Unidades especiales", 3165000, "obsdiscap_nal@unal.edu.co", 10574, "Unidad Camilo Torres (Calle 44 No. 45 - 67), Bloque B, Módulo 8, Oficina 703",
+"Los programas de Admisión Especial son creados por la Universidad Nacional de Colombia atendiendo a uno de sus fines misionales, el cual es proyectarse al territorio nacional y contribuir con la unidad nacional, la igualdad de oportunidades de acceso a la educación superior y la promoción del desarrollo de la comunidad académica nacional.");
 
-insert into programa (progID,progNombre,Area_areID) values (1,'Fomento Económico Estudiantes',1);
 
-insert into convocatoria (conv_id,progNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (1,'Fomento Económico Estudiantes','2023-01-15','2023-02-1',1,1);
-insert into convocatoria (conv_id,progNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (2,'Fomento Económico Estudiantes Alimentación','2023-01-15','2023-02-1',1,1);
-insert into convocatoria (conv_id,progNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (3,'Fomento Económico Estudiantes Alimentación','2023-01-15','2023-02-1',1,1);
-insert into convocatoria (conv_id,progNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (4,'Fomento Económico Estudiantes Alimentación','2023-01-15','2023-02-1',1,1);
-insert into convocatoria (conv_id,progNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (5,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
-insert into convocatoria (conv_id,progNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (6,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
-insert into convocatoria (conv_id,progNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (7,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
-insert into convocatoria (conv_id,progNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (8,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
-insert into convocatoria (conv_id,progNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (9,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
-insert into convocatoria (conv_id,progNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (10,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
-insert into convocatoria (conv_id,progNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (11,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
-insert into convocatoria (conv_id,progNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (12,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
-insert into convocatoria (conv_id,progNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (13,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
-insert into convocatoria (conv_id,progNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (14,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
-insert into convocatoria (conv_id,progNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (15,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
+insert into programa (progID,progNombre,Area_areID) values (1,'Fomento Económico Estudiantes',5);
+insert into bienestar.programa values
+(2, "Cita medica", 1), 
+(3, "CursoCultural", 2),
+(4, "Grupo artistico institucional", 2),
+(5, "Urgencia",  1);
+
+insert into convocatoria (conv_id,convNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (1,'Fomento Económico Estudiantes','2023-01-15','2023-02-1',1,1);
+insert into convocatoria (conv_id,convNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (2,'Fomento Económico Estudiantes Alimentación','2023-01-15','2023-02-1',1,1);
+insert into convocatoria (conv_id,convNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (3,'Fomento Económico Estudiantes Alimentación','2023-01-15','2023-02-1',1,1);
+insert into convocatoria (conv_id,convNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (4,'Fomento Económico Estudiantes Alimentación','2023-01-15','2023-02-1',1,1);
+insert into convocatoria (conv_id,convNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (5,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
+insert into convocatoria (conv_id,convNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (6,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
+insert into convocatoria (conv_id,convNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (7,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
+insert into convocatoria (conv_id,convNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (8,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
+insert into convocatoria (conv_id,convNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (9,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
+insert into convocatoria (conv_id,convNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (10,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
+insert into convocatoria (conv_id,convNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (11,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
+insert into convocatoria (conv_id,convNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (12,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
+insert into convocatoria (conv_id,convNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (13,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
+insert into convocatoria (conv_id,convNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (14,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
+insert into convocatoria (conv_id,convNombre,convFechaApertura,convFechaCierre,convEstado,Programa_progID) values (15,'Gestión Alojamiento','2023-01-15','2023-02-1',1,1);
 
 insert into fallaalimentacion (estID,fallAlcgaComida,fallAlLugar,fallAlFecha) values (23,'Almuerzo','Comedor central','2023-05-19');
 insert into fallaalimentacion (estID,fallAlcgaComida,fallAlLugar,fallAlFecha) values (9,'Desayuno','Ciencias Económicas','2023-02-12');
