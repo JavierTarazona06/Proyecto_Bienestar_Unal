fd77318d0ade3efde1ca2f699f28d297bac5b2c5
smxwx <73405701+smxwx@users.noreply.github.com>
2023-05-08 22:23:15 -0500

    Update Script_Usuarios_Bienestar_G8.sql

--- Script_Usuarios_Bienestar_G8.sql
+++ Script_Usuarios_Bienestar_G8.sql
@@ -10,3 +10,24 @@ grant select on  vw_childAna to "Ana"@"localhost";
 grant select on vw_inscripChildAna to "Ana"@"localhost";
 
 -- Tomás --
+
+#----------------------------------------------------------------------
+#                       Sebastián
+#----------------------------------------------------------------------
+create user "direccionAI"@"localhost" identified by "root";
+grant all on convocatoria to "direccionAI"@"localhost";
+grant all on proyectoestudiantil to "direccionAI"@"localhost";
+grant all on grupoproyectoestudiantil to "direccionAI"@"localhost";
+grant all on apoyogp to "direccionAI"@"localhost";
+grant all on integrantegpe to "direccionAI"@"localhost";
+grant all on actividadai to "direccionAI"@"localhost";
+grant all on participacionactividadai to "direccionAI"@"localhost";
+grant all on asesoria to "direccionAI"@"localhost";
+grant all on convocatoriapromotorconvivencia to "direccionAI"@"localhost";
+grant all on estudiante_toma_convocatoria to "direccionAI"@"localhost";
+grant all on promotorconvivencia to "direccionAI"@"localhost";
+
+create user 'estudiante'@'localhost' identified by 'root';
+grant select on vw_actividadesdisponiblesAI to "estudiante"@"localhost";
+grant select on vw_gruposproyectosest to "estudiante"@"localhost";
+grant select on convocatoriapromotorconvivencia to "estudiante"@"localhost";
