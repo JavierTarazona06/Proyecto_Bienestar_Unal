3fb2a2756009afe0eb514d0a46afc4834bc81e46
Santiago-017 <santiagobejaranoariza@gmail.com>
2023-05-29 13:33:15 -0500

    Se crean dos carpetas para los archivos de ambos grupos

---
+++ Grupo8/Script_Roles_BienestarG8.sql
@@ -0,0 +1,13 @@
+drop role if exists 'estudiante'@'localhost';
+drop role if exists 'no_estudiante'@'localhost';
+drop role if exists 'direccion_cultura'@'localhost';
+drop role if exists 'direccion_unidadEspecial'@'localhost';
+drop role if exists 'direccion_integracion'@'localhost';
+drop role if exists 'administrador'@'localhost';
+
+CREATE ROLE 'estudiante'@'localhost';
+CREATE ROLE 'no_estudiante'@'localhost';
+CREATE ROLE 'direccion_cultura'@'localhost';
+CREATE ROLE 'direccion_integracion'@'localhost';
+CREATE ROLE 'direccion_unidadEspecial'@'localhost';
+CREATE ROLE 'administrador'@'localhost';
